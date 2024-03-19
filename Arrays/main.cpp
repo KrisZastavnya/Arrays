64dba19e3c38bfcbd74e25d08632ab4b6e99cc20
Kris <bysinkakristl@gmail.com>
2024-03-19 15:23:03 +0300

    Introdaction to Arrays

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -2,7 +2,30 @@
 #include<iostream>
 using namespace std;
 
+
+
 void main()
 {
 	setlocale(LC_ALL, "");
+	const int SIZE = 5;
+	int arr[SIZE] = { 3, 5, 8 };
+	//arr[2] = 2048;          Обращение к элементу массива на запись.
+	//cout << arr[2] << endl; Обращеие к элементу массива на чтение.
+
+	//Ввод элементов массива с клавиатуры:
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << ()
+	}
+	cout << endl;
+
+
+	//Вывод элементов массива на экран:
+	/*for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;*/
+
+
 }
